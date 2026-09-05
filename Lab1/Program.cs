de985ef5a1f205f69711560a441bae5acf987827
Danil Rudko <rudkod06@gmail.com>
2026-09-05 12:01:32 +0700

    Lab1: создан проект

---
+++ Lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите n (0..20): ");
+
+        if (!int.TryParse(Console.ReadLine(), out int n )|| n < 0 || n > 20)
+        {
+            Console.WriteLine("Ошибка: нужно целое число от 0 до 20.");
+            return;
+        }
+
+        Console.WriteLine($"{n}!={Factorial(n)}");
+
+        static long Factorial(int n)
+        {
+            long result = 1;
+            for (int i = 2; i<=n; i++)
+                result = result*i; 
+            return result;
+        }
+    }
+}

23ef04eebe9ffd0405a563c1a1343eb8a74b255a
Danil Rudko <rudkod06@gmail.com>
2026-09-07 22:47:41 +0700

    Lab1: добавлено решение на задание 1

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите n (0..20): ");
+
+        if (!int.TryParse(Console.ReadLine(),out int n ) || n < 0 || n > 20)
+        {
+            Console.WriteLine("Ошибка: нужно целое число от 0 до 20.");
+            return;
+        }
+        Console.WriteLine($"{n}!={Factorial(n)}");
+
+        static long Factorial(int n)
+        {
+            long result = 1;
+            for (int i = 2; i <= n; i++)
+                result = result * i;
+            return result;
+        }
+    }
+}

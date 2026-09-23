b1cc1a303f283041404dd4a3d4b29da2f5eb40e9
Danil Rudko <rudkod06@gmail.com>
2026-09-23 13:55:57 +0700

    Lab2: добавлен базовый класс Account

--- Lab2/Bank_accounts/Program.cs
+++ Lab2/Bank_accounts/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class Account
+{
+    public string Owner { get; }
+    public decimal Balance { get; protected set; }
+
+    protected Account(string owner, decimal initialBalance = 0)
+    {
+        Owner = owner;
+        Balance = initialBalance;
+    }
+    public virtual void Deposit(decimal amount)
+    {
+        if (amount <= 0)
+            throw new ArgumentOutOfRangeException(nameof(amount), "Сумма должна быть положительной");
+        Balance += amount;
+        Console.WriteLine($"{GetType().Name} пополнение на {amount:C}. Баланс: {Balance:C}");
+    }
+
+    public abstract void Withdraw(decimal amount);
+    public abstract void AccrueInterest();
+    public override string ToString()
+    {
+        string typeName = GetType().Name;
+        string formattedBalance = string.Format("{0:C}", Balance);
+        return typeName + " (" + Owner + "): " + formattedBalance;
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+}

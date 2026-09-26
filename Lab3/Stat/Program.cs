eb5a50ef850a1149cc64bc3d11747e5db76ec552
Danil Rudko <rudkod06@gmail.com>
2026-09-26 21:29:53 +0700

    Lab3: создан класс с логикой для работы приложения

--- Lab3/Stat/Program.cs
+++ Lab3/Stat/Program.cs
@@ -1,1 +1,52 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Linq;
+
+public static class TextStat
+{
+    public static void Run(string path)
+    {
+        if (!File.Exists(path))
+        {
+            Console.WriteLine("Ошибка: файл не найден!");
+            return;
+        }
+
+        try
+        {
+            string[] lines = File.ReadAllLines(path);
+            string text = string.Join(" ", lines);
+
+            int totalChars = text.Length;
+            int charsWithoutSpaces = text.Count(c => !char.IsWhiteSpace(c));
+
+            string[] words = text.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            string longestWord = words.OrderByDescending(w => w.Length).FirstOrDefault() ?? "";
+
+            Console.WriteLine($"\n Статистика по файлу");
+            Console.WriteLine($"Путь расположения файла: {path}");
+            Console.WriteLine($"Количество строк: {lines.Length}");
+            Console.WriteLine($"Количество слов: {words.Length}");
+            Console.WriteLine($"Количество символов с пробелами: {totalChars}");
+            Console.WriteLine($"Количество символов без пробелов: {charsWithoutSpaces}");
+            Console.WriteLine($"Самое длинное слово: \"{longestWord}\" ({longestWord.Length} символов");
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine($"Ошибка ввода-вывода: {ex.Message}");
+        }
+
+        catch (UnauthorizedAccessException)
+        {
+            Console.WriteLine("Ошибка: нет прав доступа к файлу!");
+        }
+
+    }
+}
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
